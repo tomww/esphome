6b03a7ae57b0f3ffbe0a9915235fe9b2bfb34898
Thomas Wagner <1033608+tomww@users.noreply.github.com>
2026-01-04 22:32:53 +0100

    Test more logging in setup

--- esphome/components/sdp3x/sdp3x.cpp
+++ esphome/components/sdp3x/sdp3x.cpp
@@ -17,20 +17,24 @@ static const uint16_t SDP3X_STOP_MEAS = 0x3FF9;
 void SDP3XComponent::update() { this->read_pressure_(); }
 
 void SDP3XComponent::setup() {
+  ESP_LOGW(TAG, "next write: SDP3X_STOP_MEAS");
   if (!this->write_command(SDP3X_STOP_MEAS)) {
     ESP_LOGW(TAG, "Stop failed");  // This sometimes fails for no good reason
   }
 
+  ESP_LOGW(TAG, "next write: SDP3X_SOFT_RESET");
   if (!this->write_command(SDP3X_SOFT_RESET)) {
     ESP_LOGW(TAG, "Soft Reset failed");  // This sometimes fails for no good reason
   }
 
   this->set_timeout(20, [this] {
+    ESP_LOGW(TAG, "next write: SDP3X_READ_ID1");
     if (!this->write_command(SDP3X_READ_ID1)) {
       ESP_LOGE(TAG, "Read ID1 failed");
       this->mark_failed();
       return;
     }
+    ESP_LOGW(TAG, "next write: SDP3X_READ_ID2");
     if (!this->write_command(SDP3X_READ_ID2)) {
       ESP_LOGE(TAG, "Read ID2 failed");
       this->mark_failed();
